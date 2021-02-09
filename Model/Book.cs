f0a0a884de3f9f660192555a430aaa6d099b5e74
zurum ogbonda <ogbondachristian@gmail.com>
2021-02-09 14:59:19 +0100

    add dependencies

---
+++ Model/Book.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+    }
+}
